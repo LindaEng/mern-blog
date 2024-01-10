d3b262bc801f5dae2fe324ef56d200a34e29ca26
LindaEng <90lineng@gmail.com>
2024-01-10 00:53:41 -0500

    User schema added

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express")
 const app = express()
+const mongoose = require("mongoose")
+
+require("dotenv").config()
+const MONGO_URL=process.env.MONGO_URL
 
 app.use("/api", (req, res, next) => {
     res.send("Hello World")
 })
 
-app.listen(3000, () => {
-    console.log("app is listening to port 3000")
-})
+mongoose.connect(MONGO_URL).then(() => {
+    app.listen(3000, () => {
+        console.log("app is listening to port 3000")
+    })
+}).catch((error) => console.log(error))
+
